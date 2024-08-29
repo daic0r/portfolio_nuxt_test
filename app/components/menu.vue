50b4834818b20fc6a331e9d73b5590b9ad6e328b
Matthias Gruen <gruen.matthias@gmx.net>
2024-08-29 08:31:57 +0200

    Intermediate

---
+++ app/components/menu.vue
@@ -0,0 +1,15 @@
+<template>
+   <nav class="font-mono">
+      <ul class="flex space-x-4">
+         <li><NuxtLink to="/" class="link">Home</NuxtLink></li>
+         <li><NuxtLink to="/about" class="link">About</NuxtLink></li>
+      </ul>
+   </nav>
+</template>
+
+<!-- scoped means only available inside this component -->
+<style scoped>
+.link {
+   @apply p-1 hover:bg-gray-200
+}
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,11 +1,19 @@
 <template>
-   <nav>
-      <ul>
-         <li><NuxtLink to="/">Home</NuxtLink></li>
-         <li><NuxtLink to="/about">About</NuxtLink></li>
-      </ul>
-   </nav>
+   <div class="container mx-auto max-w-2xl">
+      <header class="flex justify-between items-center mt-5">
+         <div>
+            <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Matthias Gruen</NuxtLink>
+         </div>
+         <Menu />
+      </header>
+   </div>
    <div>
       <slot />
    </div>
 </template>
+
+<script setup>
+   useHead({
+      titleTemplate: '%s - Matthias Gruen'
+   })
+</script>
